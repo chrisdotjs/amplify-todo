4d848597bdaeb106fd3f56468b564ebb90e92844
“chrisdotjs” <chrisdotjs@gmail.com>
2022-01-04 17:27:04 +0100

    Remove Auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,16 +5,5 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
-    },
-    "auth": {
-        "todob76f4e4c": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
     }
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,27 +7,7 @@ const awsmobile = {
     "aws_appsync_region": "eu-west-3",
     "aws_appsync_authenticationType": "API_KEY",
     "aws_appsync_apiKey": "da2-vkyqa6n2n5cp7ipam2krgxpfa4",
-    "aws_cognito_identity_pool_id": "eu-west-3:6233ed39-7b38-4eb8-be32-f30d1ba3a74a",
-    "aws_cognito_region": "eu-west-3",
-    "aws_user_pools_id": "eu-west-3_91LkbbqJx",
-    "aws_user_pools_web_client_id": "17435tidj9ffkrklesgv0jfe3j",
-    "oauth": {},
-    "aws_cognito_username_attributes": [],
-    "aws_cognito_social_providers": [],
-    "aws_cognito_signup_attributes": [
-        "EMAIL"
-    ],
-    "aws_cognito_mfa_configuration": "OFF",
-    "aws_cognito_mfa_types": [
-        "SMS"
-    ],
-    "aws_cognito_password_protection_settings": {
-        "passwordPolicyMinLength": 8,
-        "passwordPolicyCharacters": []
-    },
-    "aws_cognito_verification_mechanisms": [
-        "EMAIL"
-    ]
+    "aws_appsync_dangerously_connect_to_http_endpoint_for_testing": true
 };
 
 
